9430e019d50150dc5d812abffa7998668b350938
trini <trini@DESKTOP-M5VG3CO>
2024-02-12 09:29:53 -0700

    Add project files.

---
+++ MyStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ArtDatabaseMuseum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtDatabaseMuseum.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                    
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
